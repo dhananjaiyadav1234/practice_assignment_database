696686191b665ddc6dc65891d5708dafc14c5865
Dhananjai <dhananjaiyadav2006@gmail.com>
2025-01-31 11:04:08 +0530

    done

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose = require('mongoose');
+require("dotenv").config();
 
 const app = express();
 const port = 3010;
-
+mongoose.connect(process.env.db_url)
+.then(() => console.log("Connected to MongoDB"))
+.catch(()=> console.log("Error connecting to database: "))
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
